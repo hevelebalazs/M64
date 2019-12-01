6f9747a17b931588f5296878404e3962c651b335
hidden1 <hevele.balazs@gmail.com>
2019-12-01 20:12:55 +0200

    basic m64 framework and sample file

--- build.bat
+++ build.bat
@@ -5,4 +5,5 @@ call vcvarsall.lnk x64
 cd bin
 del "./*" /s /q > nul
 
-cl /Od /Zi ../code/m64_windows.cpp user32.lib Gdi32.lib
+cl /Od /Zi ../code/m64.cpp
+REM cl /Od /Zi ../code/m64_windows.cpp user32.lib Gdi32.lib
--- code/m64.cpp
+++ code/m64.cpp
@@ -1,10 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+#define Assert(condition) if(!(condition)) {printf("%s (line %i) failed.\n", #condition, __LINE__); exit(-1);}
 
 #define func
 
 int
-func main()
+func main(int argument_count, char** arguments)
 {
-	printf("Hello, world!\n");
+	Assert(argument_count == 3);
+	
+	FILE *m64_file = 0;
+	fopen_s(&m64_file, arguments[1], "r");
+	Assert(m64_file != 0);
+	
+	
+	
+	fclose(m64_file);
+	
+	FILE *cpp_file = 0;
+	fopen_s(&cpp_file, arguments[2], "w");
+	Assert(cpp_file != 0);
+	
+	fprintf(cpp_file, "// Generated code.\n");
+	
+	fclose(cpp_file);
+
 	return 0;
 }
