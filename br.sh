ee4808fc6a7d9fd8e73a63869d5da2d4a86cf086
hidden1 <hevele.balazs@gmail.com>
2023-08-18 22:19:54 +0200

    added c compilation and running exe to sh file

--- br.sh
+++ br.sh
@@ -1,5 +1,14 @@
 #Build and Run
 gcc M64.c -o M64.exe
 if [ $? == 0 ] ; then
-		./M64.exe Example/ToC.m64 Example/ToC.m64.c
+	./M64.exe Example/ToC.m64 Example/ToC.m64.c
+fi
+if [ $? == 0 ] ; then
+	echo Compiling c code...
+	gcc Example/ToC.m64.c -o Example/ToC.exe
+fi
+if [ $? == 0 ] ; then
+	echo Running exe...
+	echo --------------
+	./Example/ToC.exe
 fi
