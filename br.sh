ac37a5a3e0280e2d16bb5b3df04c44d8da4ccc49
hidden1 <hevele.balazs@gmail.com>
2023-08-18 22:23:08 +0200

    sh file beautification

--- br.sh
+++ br.sh
@@ -1,14 +1,23 @@
 #Build and Run
 gcc M64.c -o M64.exe
-if [ $? == 0 ] ; then
-	./M64.exe Example/ToC.m64 Example/ToC.m64.c
+if [ $? != 0 ] ; then
+	exit 1
 fi
-if [ $? == 0 ] ; then
-	echo Compiling c code...
-	gcc Example/ToC.m64.c -o Example/ToC.exe
+
+./M64.exe Example/$1.m64 Example/$1.m64.c
+
+if [ $? != 0 ] ; then
+	exit 1
 fi
-if [ $? == 0 ] ; then
-	echo Running exe...
-	echo --------------
-	./Example/ToC.exe
+
+echo Compiling c code...
+gcc Example/$1.m64.c -o Example/$1.exe
+
+if [ $? != 0 ] ; then
+	exit 1
 fi
+
+echo Running exe...
+echo --------------
+./Example/$1.exe
+
